f0e2c7c803194e5794fe0090944e9a619dedaacb
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-01-28 20:30:20 -0500

    implemeted edit returns

--- src/api/setup.py
+++ src/api/setup.py
@@ -1,38 +1,78 @@
 
-map = """map = {}\n"""
+# "map = {}\\n"
 # maps parameter to node
-# a node is defined as (rv, isBase, children)
+# a node is defined as (rv, children)
 # where 
 #   rv = return value
-#   isBase = boolean indicating if function returns base case
-#   children = list of child parameters (if not base case)
+#   children = list of child parameters
+map = "qTY8eDfs9 = {}\\n"
 
-def find_function_name(code):
-    name = ""
-    keyword = "\\ndef "
-    inComment = found = False
 
+t = """\\n# fibonacci sequence\\ndef fun(num):\\n    if (n == 0 or n == 1):\\n        return 1\\n    else:\\n        return fun(n-1) + fun(n-2)\\nprint("Result: " + str(fun(4)))"""
+
+
+def insert_lines(code, rv, tab, start, end):
+    var_line = tab + "jB2h3dCi1 = " + rv + "\\n"
+    map_line = tab + "qTY8eDfs9[n] = (jB2h3dCi1, True, [])\\n"
+    return_line = tab + "return " + "jB2h3dCi1"
+
+    start = code[:start]
+    end = code[end:]
+    code = start + var_line + map_line + return_line + end
+
+    # number of characters inserted
+    s = len(var_line) + len(map_line) + len(return_line) - 7 - len(rv) - len(tab)
+
+    return (code, s)
+
+
+def find_returns(code):
+    keyword = "return "
+    n = len(keyword)
+    indices = []
     i = 0
-    while i < len(code):
-        if inComment:
-            if code[i:i+1] != "\\n":
-                inComment = False
-        else:
-            if (code[i] == '#'):
-                inComment = True
-            elif (code[i:i+6] == keyword):
-                found = True
-                break
+    while i+n < len(code):
+        if code[i:i+n] == keyword:
+            indices.append(i)
         i += 1
+    return indices
 
-    if not found:
-        return False
-    i += 6
-    while i < len(code) and code[i] != '(':
-        name += code[i]
-        i += 1
-    return name
-       
-            
+# edit every return statement so we replace
+#   return <return_value>
+# with
+#   a = <return_value>
+#   map[n] = (a, [<p1>, ... ,<pn>])
+#   return a
+def edit_returns(code, indices):
+    for i in range(len(indices)):
+        idx = indices[i]
+        tab = ""
+        j = idx
+        while j >= 0 and code[j-2:j] != "\\n": 
+            tab = tab + " "
+            j -= 1
+        
+        rv = ""
+        k = idx+7
+        print(len(tab))
+        while k+2 < len(code) and code[k:k+2] != "\\n":
+            rv += code[k]
+            k += 1
+        print(rv)
+        c = insert_lines(code, rv, tab, j, k);
+        code, s = c[0], c[1]
+
+        for l in range(len(indices)):
+            if (l > i):
+                indices[l] += s
+    
+    return code
+        
+
 def setup(code):
+    indices = find_returns(code)
+    code = edit_returns(code, indices)
     code = map + code
+    print(code)
+
+setup(t)
