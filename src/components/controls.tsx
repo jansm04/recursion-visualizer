a6d676625a1881e99cc726fd8fddb1d618b1541a
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-02-04 15:00:03 -0500

    added loading text

--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -2,19 +2,24 @@ import { RefObject } from "react"
 
 const Controls = ({
     onRunCode,
-    runButtonRef
+    runButtonRef,
+    isLoading
 }:{
     onRunCode: () => Promise<void>,
-    runButtonRef: RefObject<HTMLButtonElement>
+    runButtonRef: RefObject<HTMLButtonElement>,
+    isLoading: boolean
 }) => {
     return (
         <div className="bg-[#161616]">
-            <button 
-                ref={runButtonRef}
-                className='h-fit w-20 m-2 text-center border rounded text-white hover:bg-[#353535]' 
-                onClick={onRunCode}>
-                    Run
-            </button>
+            <div className="inline-block">
+                <button 
+                    ref={runButtonRef}
+                    className='h-fit w-20 m-2 text-center border rounded text-white hover:bg-[#353535]' 
+                    onClick={onRunCode}>
+                        Run
+                </button>
+            </div>
+            { isLoading ? <div className="ml-6 m-2 inline-block">one sec...</div> : <></> }
         </div>
         
     )
