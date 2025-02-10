74c62aa65b27276596d8a64d50722102fa07b5cd
jansm04 <jan.smailbegovic@gmail.com>
2025-02-10 15:07:51 -0800

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,13 @@ import Link from "next/link";
 const Footer = () => {
     return (
         <div className="p-6 bg-[#161616] text-gray-500 text-center text-[14px]">
-            Made by @jansm04 in 2024&nbsp;&nbsp;•&nbsp;&nbsp; 
-            {<Link href='https://github.com/jansm04/recursion-visualizer' target="_blank">
-                <div className="text-sky-800 inline">Github</div>
+            Made by {
+                <Link href="https://www.janbegovic.com/" target="_blank">
+                    <div className="text-sky-600 inline"> Jan Smailbegovic </div>
+                </Link>
+            } in 2024&nbsp; •&nbsp; 
+            {<Link href='https://github.com/jansm04/dav' target="_blank">
+                <div className="text-sky-600 inline"> GitHub </div>
             </Link>}
         </div>
     )
